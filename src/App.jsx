590f25492ff4bb883174881c7e8ae8d7d7cee3a1
Ndong Ngoua Andrew Keci <andrewkeci.dev@gmail.com>
2026-08-02 19:59:42 +0100

    Initialisation du Projet MarchéBio frontend

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Bonjour à tous</h1>
+            <p className="py-6">
+              Le projet MarchéBio vise à mettre en relation directe les producteurs et les acheteurs à travers une plateforme web simple et intuitive. 
+              Cette solution permettra aux producteurs de publier leurs produits et aux consommateurs de les consulter puis de passer commande facilement.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
