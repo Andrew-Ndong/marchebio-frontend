6dcdd4edc42e2407f8624c1f7e11b47e631efced
Ndong Ngoua Andrew Keci <andrewkeci.dev@gmail.com>
2026-08-03 15:31:56 +0100

    Modification : Ajout du routeur vers page home et login

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Public/Home'
+import Login from './pages/Public/Login'
 
 function App() {
-
   return (
     <>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Bonjour à tous</h1>
-            <p className="py-6">
-              Le projet MarchéBio vise à mettre en relation directe les producteurs et les acheteurs à travers une plateforme web simple et intuitive. 
-              Cette solution permettra aux producteurs de publier leurs produits et aux consommateurs de les consulter puis de passer commande facilement.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/home' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
