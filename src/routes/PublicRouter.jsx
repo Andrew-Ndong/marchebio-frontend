b70f2a5cf613894d0f102c4fb6dc5c3dc7529d53
Ndong Ngoua Andrew Keci <andrewkeci.dev@gmail.com>
2026-08-04 19:19:02 +0100

    Modification : Ajout de la route login et du layoutpublic pour la mise en page

--- src/routes/PublicRouter.jsx
+++ src/routes/PublicRouter.jsx
@@ -1,20 +1,26 @@
 import { Route, Routes } from 'react-router'
 
 import { Home, Produits, Services, Blog } from '@/pages/Public'
-import Error from '../_utils/Error'
+import Error from '@/utils/Error'
+import Login from '@/pages/Auth/Login'
+import Layoutpublic from '@/layouts/Layoutpublic'
 
 const PublicRouter = () => {
   return (
     <>
       <Routes>
-        <Route index element={<Home />} />
+        <Route element={<Layoutpublic />}>
+          <Route index element={<Home />} />
 
-        <Route path='/home' element={<Home />} />
-        <Route path='/produits' element={<Produits />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/blog' element={<Blog />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/produits' element={<Produits />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/auth/login' element={<Login />} />
 
-        <Route path='*' element={<Error />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+
       </Routes>
     </>
   )
